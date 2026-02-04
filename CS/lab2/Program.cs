ac8b6a4daf4e93c540168e9904a67014024deec5
Barebuh8481 <Barebuh8841@gmail.com>
2026-02-05 01:53:22 +0500

    lab2 cs&py&conclusion

---
+++ CS/lab2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace game;
+
+public class Unit
+{
+    public string Name;
+    public int Hp;
+
+
+    public Unit(string name, int hp)
+    {
+        Name = name;
+        Hp = hp;
+    }
+    //виртуал позволяет переопределять
+    public virtual void Attack()
+    {
+        Console.WriteLine($"{Name} атакует");
+    }
+}
+//наследование от юнита через двоеточие
+public class Archer : Unit
+{
+    public Archer(string name, int hp) : base(name, hp)
+    {
+    }
+
+    //переопределение через оверрайд
+    public override void Attack()
+    {
+        Console.WriteLine($"{Name} стреляет из лука");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Archer archer = new Archer("Лучник", 100);
+        archer.Attack();
+        Unit warior = new Unit("Воин", 150);
+        warior.Attack();
+    }
+}
---
+++ PY/lab2py/lab2py.py
@@ -0,0 +1,17 @@
+﻿class Unit:
+    #конструктор
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+
+    def attack(self):
+        print(f'{self.name} атакует')
+
+#наследование от Unit
+class Archer(Unit):
+    def attack(self):
+        print(f'{self.name} стреляет из лука')
+
+if __name__ == "__main__":
+    hero = Archer("Лучник", 100)
+    hero.attack()
