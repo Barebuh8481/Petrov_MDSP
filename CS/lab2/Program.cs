90bbd4ba5d886bd4a59ceb31f8b5c3467d22de91
Barebuh8481 <Barebuh8841@gmail.com>
2026-02-19 09:26:20 +0500

    новые функции

--- CS/lab2/Program.cs
+++ CS/lab2/Program.cs
@@ -7,26 +7,44 @@ public class Unit
     public string Name;
     public int Hp;
 
-
     public Unit(string name, int hp)
     {
         Name = name;
         Hp = hp;
     }
-    //виртуал позволяет переопределять
+
+    // виртуал позволяет переопределять
     public virtual void Attack()
     {
         Console.WriteLine($"{Name} атакует");
     }
+
+    // отнимает здоровье
+    public void TakeDamage(int damage)
+    {
+        Hp -= damage;
+        if (Hp < 0)
+        {
+            Hp = 0; // чтобы в минус не ушло
+        }
+        Console.WriteLine($"{Name} получает {damage} урон, осталось {Hp} hp");
+    }
+
+    //жив или нет
+    public bool IsAlive()
+    {
+        return Hp > 0;
+    }
 }
-//наследование от юнита через двоеточие
+
+    // наследование от юнита через двоеточие
 public class Archer : Unit
 {
     public Archer(string name, int hp) : base(name, hp)
     {
     }
 
-    //переопределение через оверрайд
+    // переопределение через оверрайд
     public override void Attack()
     {
         Console.WriteLine($"{Name} стреляет из лука");
@@ -39,7 +57,18 @@ class Program
     {
         Archer archer = new Archer("Лучник", 100);
         archer.Attack();
-        Unit warior = new Unit("Воин", 150);
-        warior.Attack();
+
+        //новые функции
+        archer.TakeDamage(40);
+        archer.TakeDamage(70);
+
+        if (archer.IsAlive())
+        {
+            Console.WriteLine($"{archer.Name} жив");
+        }
+        else
+        {
+            Console.WriteLine($"{archer.Name} погиб");
+        }
     }
 }
--- PY/lab2py/lab2py.py
+++ PY/lab2py/lab2py.py
@@ -7,6 +7,17 @@
     def attack(self):
         print(f'{self.name} атакует')
 
+    #отнимает здоровье
+    def take_damage(self, damage):
+        self.hp -= damage
+        if self.hp < 0:
+            self.hp = 0
+        print(f'{self.name} получает {damage} урона осталось hp: {self.hp}')
+
+    #возврат здоровья
+    def is_alive(self):
+        return self.hp > 0
+
 #наследование от Unit
 class Archer(Unit):
     def attack(self):
@@ -15,3 +26,12 @@ class Archer(Unit):
 if __name__ == "__main__":
     hero = Archer("Лучник", 100)
     hero.attack()
+    
+    #новые функции
+    hero.take_damage(40)
+    hero.take_damage(70)
+    
+    if hero.is_alive():
+        print(f"{hero.name} жив")
+    else:
+        print(f"{hero.name} погиб")
