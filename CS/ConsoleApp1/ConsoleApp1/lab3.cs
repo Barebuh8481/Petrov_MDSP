f8a08e84a460de919dc2ca0116ef890cf9286488
Barebuh8481 <Barebuh8841@gmail.com>
2026-02-19 11:42:37 +0500

    ыфвфыв

---
+++ CS/ConsoleApp1/ConsoleApp1/lab3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.Office.Interop.Excel;
+
+namespace Lab3
+{
+    class lab3
+    {
+        static void Main(string[] args)
+        {
+            //COM объект
+            Application excelApp = new Application();
+            if (excelApp == null)
+            {
+                return;
+            }
+
+            string filePath = @"C:\Users\Barebuh\source\repos\Barebuh8481\Petrov_MDSP\a.xlsx";
+
+            //книгу и получаем первый лист
+            Workbook excelBook = excelApp.Workbooks.Open(filePath);
+            _Worksheet excelSheet = excelBook.Sheets[1];
+            Microsoft.Office.Interop.Excel.Range excelRange = excelSheet.UsedRange;
+
+            // количество строк и колонок
+            int rows = excelRange.Rows.Count;
+            int cols = excelRange.Columns.Count;
+
+            //выводим данные 
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= cols; j++)
+                {
+                    if (excelRange.Cells[i, j] != null && excelRange.Cells[i, j].Value2 != null)
+                    {
+                        //значение ячейки
+                        Console.Write(excelRange.Cells[i, j].Value2.ToString() + "\t| ");
+                    }
+                }
+                Console.WriteLine(); // Переход на новую строку
+            }
+
+            excelBook.Close(false);
+            excelApp.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PY/lab3.py
@@ -0,0 +1,23 @@
+﻿import pandas as pd
+
+print("чтение с параметрами")
+# sheet_name='' лист
+# usecols="A,D" - Дата и Выручка
+# skiprows=1 - пропускаем первую строку
+# nrows=3 - читаем только 3 строки
+# names - задаем свои имена колонка
+df_params = pd.read_excel('a.xlsx', 
+                          sheet_name='Продажи', 
+                          usecols="A,D", 
+                          skiprows=1, 
+                          nrows=3,
+                          names=['Дата операции', 'Доход'])
+print(df_params)
+
+
+print("чтение сразу нескольких листов")
+# список листов
+all_sheets = pd.read_excel('a.xlsx', sheet_name=['Продажи', 'Расходы'])
+
+print("\nДанные с листа 'Расходы':")
+print(all_sheets['Расходы'].head()) #первые строки второго листа
