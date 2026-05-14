61293a1f6bcbd5cd5ccc66f81bb0f630248ce5e2
Barebuh8481 <Barebuh8841@gmail.com>
2026-05-14 10:54:22 +0500

    FF

---
+++ CS/lab8/Client/Client/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+namespace PipeClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Клиент");
+            string ss = "";
+
+            // именованный канал клиентом (подключется к "Pipe")
+            using (NamedPipeClientStream pipestream = new NamedPipeClientStream(".", "Pipe", PipeDirection.InOut))
+            {
+                //Соединение клиента с каналом
+                pipestream.Connect();
+                Console.WriteLine("Успешно подключились к серверу!\n");
+
+                using (StreamReader reader = new StreamReader(pipestream))
+                using (StreamWriter writer = new StreamWriter(pipestream))
+                {
+                    writer.AutoFlush = true;
+                    int fl = 0;
+
+                    // обмен
+                    do
+                    {
+                        if (fl == 0)
+                        {
+                            ss = reader.ReadLine();
+                            Console.WriteLine("сообщение от сервера: " + ss);
+                            fl = 1; // очередь клиента
+                        }
+                        else
+                        {
+                            Console.Write("сообщение от клиента ? ");
+                            ss = Console.ReadLine();
+                            writer.WriteLine(ss);
+                            fl = 0; // очередь сервера
+                        }
+                    }
+                    while (ss != "ДОМОЙ");
+                }
+            }
+            Console.WriteLine("Конец сеанса");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CS/lab8/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+namespace PipeServer
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Сервер");
+            // создание канала сервером
+            using (NamedPipeServerStream pipestream = new NamedPipeServerStream("Pipe"))
+            {
+                Console.WriteLine("Ожидание клиента");
+                pipestream.WaitForConnection();
+                Console.WriteLine("Клиент подключен \n");
+
+                using (StreamReader reader = new StreamReader(pipestream))
+                using (StreamWriter writer = new StreamWriter(pipestream))
+                {
+                    writer.AutoFlush = true; // чтобы сообщения сразу приходили
+                    string ss = "";
+                    int fl = 0; // поочередный обмен (0 - сервер, 1 - клиент)
+
+                    // 3. Обмен данными до слова "ДОМОЙ
+                    while (ss != "ДОМОЙ")
+                    {
+                        if (fl == 0)
+                        {
+                            Console.Write("Ваше сообщение ? ");
+                            ss = Console.ReadLine();
+                            writer.WriteLine(ss);
+                            fl = 1; // передаем ход клиенту
+                        }
+                        else
+                        {
+                            ss = reader.ReadLine();
+                            Console.WriteLine("Получено сообщение от клиента: " + ss);
+                            fl = 0; // наш ход
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Конец сеанса");
+            Console.ReadLine();
+        }
+    }
+}
